0f177d42a976f7be39115d79efb25a4a03c6f685
KaKETHEBegginer <rangu1rangila@gmail.com>
2026-08-13 18:26:30 +0000

    power: smb5: Enable reliable Bypass Charging via user voter and charging_enabled sysfs

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -2829,32 +2829,36 @@ static int smblib_therm_charging(struct smb_charger *chg)
 int smblib_set_prop_battery_charging_enabled(struct smb_charger *chg,
 			  const union power_supply_propval *val)
 {
-  int rc;
+	int rc = 0;
 
-  smblib_dbg(chg, PR_MISC, "%s intval= %x\n",__FUNCTION__,val->intval);
+	if (!chg)
+		return -EINVAL;
 
-  if (1 == val->intval) {
-	  rc = smblib_masked_write(chg, CHARGING_ENABLE_CMD_REG,
-		  CHARGING_ENABLE_CMD_BIT,CHARGING_ENABLE_CMD_BIT);
-	  if (rc < 0) {
-		  smblib_err(chg, "Couldn't enable charging rc=%d\n",
-					  rc);
-		  return rc;
-	  }
-  }
-  else if (0 == val->intval) {
-	  rc = smblib_masked_write(chg, CHARGING_ENABLE_CMD_REG,
-		  CHARGING_ENABLE_CMD_BIT,0);
-	  if (rc < 0) {
-		  smblib_err(chg, "Couldn't disable charging rc=%d\n",
-					  rc);
-		  return rc;
-	  }
-  }
-  else
-	  smblib_err(chg, "Couldn't disable charging rc=%d\n",rc);
+	if (chg->chg_disable_votable) {
+		rc = vote(chg->chg_disable_votable, USER_VOTER, !val->intval, 0);
+		if (rc < 0)
+			smblib_err(chg, "Couldn't vote to %s charging rc=%d\n",
+				val->intval ? "enable" : "disable", rc);
+	}
 
-  return 0;
+	if (val->intval) {
+		rc = smblib_masked_write(chg, CHARGING_ENABLE_CMD_REG,
+			CHARGING_ENABLE_CMD_BIT, CHARGING_ENABLE_CMD_BIT);
+		if (rc < 0) {
+			smblib_err(chg, "Couldn't enable charging rc=%d\n", rc);
+			return rc;
+		}
+	} else {
+		rc = smblib_masked_write(chg, CHARGING_ENABLE_CMD_REG,
+			CHARGING_ENABLE_CMD_BIT, 0);
+		if (rc < 0) {
+			smblib_err(chg, "Couldn't disable charging rc=%d\n", rc);
+			return rc;
+		}
+	}
+
+	power_supply_changed(chg->batt_psy);
+	return 0;
 }
 extern union power_supply_propval lct_therm_lvl_reserved;
 extern bool lct_backlight_off;
