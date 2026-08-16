0cec65576c1777d982e0d4e30f9d77de0f7a861d
KaKe-567i <botwheel973@gmail.com>
2026-08-16 07:05:08 +0000

    ufs: Extend performance clock gating delay to 100ms for continuous UFS 2.1 game asset reading

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -248,7 +248,7 @@ static void ufshcd_update_uic_error_cnt(struct ufs_hba *hba, u32 reg, int type)
 #define UFSHCD_REF_CLK_GATING_WAIT_US 100 /* microsecs */
 
 #define UFSHCD_CLK_GATING_DELAY_MS_PWR_SAVE	10
-#define UFSHCD_CLK_GATING_DELAY_MS_PERF		50
+#define UFSHCD_CLK_GATING_DELAY_MS_PERF		100
 
 /* IOCTL opcode for command - ufs set device read only */
 #define UFS_IOCTL_BLKROSET      BLKROSET
