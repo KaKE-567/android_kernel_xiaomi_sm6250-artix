48e947ead9daf0eb5e0a6377e7b42170d7cfe4b9
kake <kake@build.local>
2026-08-26 04:14:13 +0000

    msm: adreno: Spoof GPU model to Adreno 650 for POCO F3 flagship profile

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -3888,13 +3888,7 @@ static void adreno_regulator_disable_poll(struct kgsl_device *device)
 static void adreno_gpu_model(struct kgsl_device *device, char *str,
 				size_t bufsz)
 {
-	struct adreno_device *adreno_dev = ADRENO_DEVICE(device);
-
-	snprintf(str, bufsz, "Adreno%d%d%dv%d",
-			ADRENO_CHIPID_CORE(adreno_dev->chipid),
-			 ADRENO_CHIPID_MAJOR(adreno_dev->chipid),
-			 ADRENO_CHIPID_MINOR(adreno_dev->chipid),
-			 ADRENO_CHIPID_PATCH(adreno_dev->chipid) + 1);
+	snprintf(str, bufsz, "Adreno650v1");
 }
 
 static int adreno_suspend_device(struct kgsl_device *device,
